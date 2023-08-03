9c7b2c14ff06c645d27f0886752577f601d163b5
nguyennamthien <nguyennamthien00@gmail.com>
2023-08-03 12:34:32 +0700

    detaigiuaky2

---
+++ lib/const.dart
@@ -0,0 +1,29 @@
+// ignore: non_constant_identifier_names
+List<String> ImagesList = [
+  'images/islambad.jpg',
+  'images/karachi.jpg',
+  'images/lahore.jpg',
+  'images/multan.jpg',
+  'images/muree.jpg',
+  'images/swat.jpg',
+  'images/81.jpg'
+];
+// ignore: non_constant_identifier_names
+List<String> CityList = [
+  'Islambad',
+  'Karachi',
+  'Lahore',
+  'Multan',
+  'Muree',
+  'Swat',
+  'Larmat81',
+];
+
+// ignore: non_constant_identifier_names
+List<String> SuggestList = [
+  'Most visited',
+  'Hotels',
+  'Atms',
+  'Food',
+  'Best Places',
+];
